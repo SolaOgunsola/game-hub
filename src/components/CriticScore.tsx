e3b8fa04c09251b43dc6e2e00c116160b37fea0c
Olusola Ogunsola <ogunsolao@fullcount.net>
2023-08-06 19:56:23 -0500

    Build criticScore component

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,18 @@
+import { Badge } from '@chakra-ui/react';
+import React from 'react';
+
+interface Props {
+    score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+    let color = score > 90 ? 'green' : score > 50 ? 'yellow' : 'red';
+
+    return (
+        <Badge fontSize='14px' colorScheme={color} paddingX={2}>
+            {score}
+        </Badge>
+    );
+};
+
+export default CriticScore;
