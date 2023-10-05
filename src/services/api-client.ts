7927ac682f97acbb68a22fcbc66c9576da0b7cce
SolaOgunsola <so.ogunsola@gmail.com>
2023-10-05 09:26:45 -0500

    Build reusable api client

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,13 +1,30 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
 export interface FetchResponse<T> {
     count: number;
     results: T[];
 }
 
-export default axios.create({
+const axiosInstance = axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
         key: '0c3544fc99424f5f93a72f50fd56779d',
     },
 });
+
+class APIClient<T> {
+    endpoint: string;
+
+    constructor(endpoint: string) {
+        this.endpoint = endpoint;
+    }
+
+    getAll = (config: AxiosRequestConfig) => {
+        return axiosInstance
+            .get<FetchResponse<T>>(this.endpoint, config)
+            .then(response => response.data);
+    }
+
+}
+
+export default APIClient;
